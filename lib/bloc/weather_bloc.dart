2df074070e100055280ad477d6ad882957ce9256
aj17i <ahmad.y.jarouche06@gmail.com>
2024-02-25 14:22:44 +0200

    The Weather app

---
+++ lib/bloc/weather_bloc.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:weather/weather.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/data/my_data.dart';
+
+part 'weather_event.dart';
+
+part 'weather_state.dart';
+
+class WeatherBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
+  WeatherBloc() : super(WeatherBlocInitial()) {
+    on<FetchWeather>((event, emit) async {
+      emit(WeatherBlocLoading());
+      try {
+        WeatherFactory wf = WeatherFactory(API_KEY, language: Language.ENGLISH);
+
+        Weather weather = await wf.currentWeatherByLocation(
+          event.position.latitude,
+          event.position.longitude,
+        );
+        emit(WeatherBlocSuccess(weather));
+      } catch (e) {
+        emit(WeatherBlocFailure());
+      }
+    });
+  }
+}
---
+++ lib/data/my_data.dart
@@ -0,0 +1,1 @@
+String API_KEY = "4ebd5e02c8f56837cedd80b42f02886e";
